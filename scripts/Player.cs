9837bdfff033ef0d81d305c4f5ad7bd6cfaca599
JokeUrSelf <81817136+JokeUrSelf@users.noreply.github.com>
2022-08-30 08:48:54 +0200

    Added C# support

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -3,15 +3,26 @@ using System;
 
 public class Player : KinematicBody2D
 {
-	// Declare member variables here. Examples:
-	// private int a = 2;
-	// private string b = "text";
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		
-	}
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        GD.Print("asas");
+    }
+
+
+
+    const int maxJumpHeight = 64*12*3;
+
+    public override void _PhysicsProcess(float delta){
+
+
+        MoveAndSlide(maxJumpHeight,Vector2.Up);
+    }
+    
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
 //  public override void _Process(float delta)
