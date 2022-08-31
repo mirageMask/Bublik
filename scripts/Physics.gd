a1b6d0c2437e6f8959339829948f6172c9f81197
JokeUrSelf <81817136+JokeUrSelf@users.noreply.github.com>
2022-08-31 19:35:33 +0200

    basic animation binding

--- scripts/Physics.gd
+++ scripts/Physics.gd
@@ -3,12 +3,12 @@ extends KinematicBody2D
 const PX_HEIGHT:=12
 const FPS = 60
 
-onready var sprite := $Sprite
+onready var animPlayer := $AnimationPlayer
+onready var animSprite := $AnimatedSprite
 
-
-export var movement_speed:=20
-export var jump_height_in_bodies:=2
-export var jump_duration:=.5
+export var movement_speed:=30
+export var jump_height_in_bodies:=3
+export var jump_duration:=.3
 
 var jump_max_height:float = PX_HEIGHT * jump_height_in_bodies / jump_duration
 
@@ -17,15 +17,24 @@ var gravity:Vector2 = Vector2(.0, jump_max_height / FPS / jump_duration)
 var vect:=Vector2.ZERO
 
 func _physics_process(delta):
-		
-	vect.x = 0 ;
-	if Input.is_action_just_pressed("jump"):
+	vect.x = 0
+
+	if is_on_floor() and vect.x == 0:
+		animPlayer.play("idle")
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		vect.y -= jump_max_height
-		sprite.timing(.2, "anim_idle")
+		animPlayer.play("jump")
 	if Input.is_action_pressed("ui_left"):
 		vect.x =- movement_speed
-		sprite.timing(.2, "anim_idle")
+		animSprite.flip_h = true
+		animPlayer.play("walk")
+
 	if Input.is_action_pressed("ui_right"):
 		vect.x =+ movement_speed
+		animSprite.flip_h = false
+		animPlayer.play("walk")
 
+	
+
+		
 	vect = move_and_slide(vect + gravity, Vector2.UP)
